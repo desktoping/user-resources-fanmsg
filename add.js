34d875e40b253481aee7a1c9105e6b7eb7b78fb1
unknown <desktoping@github.com>
2015-05-08 21:11:30 +0800

    this sir

--- add.js
+++ add.js
@@ -1,37 +1,55 @@
-var app = require('koa')();
-var router = require('koa-route');
+// initializing global variables
+var koa = require('koa');
+var app = koa();
+var mount = require('koa-mount');
+var router = require('koa-router');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/user2');
+var query = "";
 
+// initializing variables
 var userSchema = new mongoose.Schema({
 	user: String,
 	pass: String
 });
-
 var Model = mongoose.model('user2',userSchema);
+var testRoute = new router();
 var Query = new Model({user: "red", pass: "1234"});
-console.log(Query);
 
+// custom functions
 var print = function *(d){
 		yield d;
 		console.log(d);
 		this.type = 'json';
 		this.status = 200;
-		app.body = 'Welcome %s',d;
+		this.body = 'Welcome %s',d;
 };
-print('g')
 var callback = function (err, data) {
-	var string = data;
-	//console.log(string);
+	query = data;
+	//console.log(query);
 	if (err) return console.error(err);
 	else
 		console.log('retrieving...');
-		print(string);
+		print(query);
 		console.log('end retieve.');
 };
-  
-var t = Model.find({pass: '1234' } , callback);
-//console.log(t);
+var handler = function *(next){
+    app.body = {'Welcome': 'This is a level 2 Hello World Application!!'};
+	console.log(app);
+	// mongoose code to display current users lists
+};
+
+//transforming page
+testRoute.get('/users', function *(){
+	this.body = ('Welcome2');
+    var data = yield Model.find({pass: '1234'});
+	console.log(data);
+	this.body = data;
+});
+testRoute.get('/index', handler);
+app.use(mount('/users', testRoute.middleware()));
+
+//creating page
 app.listen(8000);
 // adD.save(function (err) {
   // if (err) {
