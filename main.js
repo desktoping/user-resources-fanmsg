fb900aaf45312743482be990ca936e47ba7a6821
unknown <desktoping@github.com>
2015-05-09 22:16:42 +0800

    integration

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 var app = require('koa')();
 var mongoose = require('mongoose');
-var router = require('koa-router')();
+var router = require('koa-router');
 var parser = require('koa-bodyparser');
 mongoose.connect('mongodb://localhost/user2');
 var usersController = require('./user-controller');
-var userSchema = new mongoose.Schema({
-	user: String,
-	pass: String
-});
 
 usersController(router);
-
-
-
+app.use(parser());
 app.use(router.routes());
 app.listen(8000);
